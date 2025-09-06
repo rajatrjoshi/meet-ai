87da6a08878e53283b86107b54bf58cffd172792
rajatrjoshi <rajatj969@gmail.com>
2025-09-06 17:36:46 +0530

    setup drizzle and neon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="text-4xl">
-        Hello World!
-    </div>
+    <Button variant="destructive">
+      Click me!
+    </Button>
   );
 }
