26f6a95ad29c5270315cb2ebce471ec3e6466e4c
rajatrjoshi <rajatj969@gmail.com>
2025-09-07 09:47:31 +0530

    added signup functionality

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {email, z} from "zod";
+import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
